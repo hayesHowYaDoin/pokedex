b3c7eaa56a79159f97a6b7edfd2accf257fb3bca
Jordan Hayes <jordanhayes98@gmail.com>
2024-06-20 01:15:03 +0000

    Moved layout into constant

--- src/infrastructure/tui/pages/list.rs
+++ src/infrastructure/tui/pages/list.rs
@@ -5,36 +5,35 @@ use ratatui::{
 use std::io::Result;
 use std::cell::LazyCell;
 
-use crate::infrastructure::tui::widgets::InputBox;
-
 const SEARCH_WIDGET_LAYOUT_IDX: usize = 0;
 const LIST_WIDGET_LAYOUT_IDX: usize = 1;
 
+const LAYOUT: LazyCell<Layout> = 
+    LazyCell::new(|| Layout::default()
+        .direction(Direction::Vertical)
+        .constraints([
+            Constraint::Length(3),
+            Constraint::Min(5),
+        ]));
+
 pub struct ListPage<'a> {
-    search_widget: InputBox<'a>,
+    search_widget: Paragraph<'a>,
     list_widget: Paragraph<'a>,
 }
 
 impl ListPage<'_> {
     pub fn new() -> Self {
-        let search_widget = InputBox::new(Block::bordered()
-            .title("Search"));
+        let search_widget = Paragraph::new("Enter search query here!")
+            .block(Block::bordered().title("Search"));
         let list_widget = Paragraph::new("Hello Ratatui! (press 'q' to quit)")
-            .block(Block::bordered().title("All the Pokemon I et:"));
+            .block(Block::bordered());
 
         Self {search_widget, list_widget}
     }
 
     pub fn render<B: Backend>(&self, terminal: &mut Terminal<B>) -> Result<()> {
-        let layout: LazyCell<Layout> = LazyCell::new(|| Layout::default()
-            .direction(Direction::Vertical)
-            .constraints([
-                Constraint::Percentage(10),
-                Constraint::Percentage(90),
-            ]));
-
         terminal.draw(|frame| {
-            let layout = layout.split(frame.size());
+            let layout = LAYOUT.split(frame.size());
 
             frame.render_widget(&self.search_widget, layout[SEARCH_WIDGET_LAYOUT_IDX]);
             frame.render_widget(&self.list_widget, layout[LIST_WIDGET_LAYOUT_IDX]);
