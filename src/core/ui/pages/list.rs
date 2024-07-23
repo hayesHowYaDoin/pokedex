b859ef1b2ec6f7c6889e668c12a710c36e82ea94
Jordan Hayes <jordanhayes98@gmail.com>
2024-07-23 14:15:25 +0000

    Updated unit teset for ListPage

--- src/core/ui/pages/list.rs
+++ src/core/ui/pages/list.rs
@@ -16,72 +16,16 @@ impl ListPage {
             list_widget: PokemonTable::new(pokemon, 0),
         }
     }
-
-    pub fn get_selected(&self) -> Pokemon {
-        self.list_widget.get_selected()
-    }
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use cascade::cascade;
-
-//     use super::*;
+#[cfg(test)]
+mod tests {
+    use super::*;
     
-//     #[test]
-//     fn test_update_new_character() {
-//         let mut page = ListPage::new(10);
-//         let event = Event::NewCharacter('A');
-        
-//         page.update(&event);
-        
-//         assert_eq!(page.search_widget.text(), "A");
-//     }
-    
-//     #[test]
-//     fn test_update_delete_character() {
-//         let page = cascade! {
-//             ListPage::new(10);
-//             ..update(&Event::NewCharacter('A'));
-//             ..update(&Event::DeleteCharacter);
-//         };
-        
-//         assert_eq!(page.search_widget.text(), "");
-//     }
-    
-//     #[test]
-//     fn test_update_up() {
-//         let page = cascade! {
-//             ListPage::new(10);
-//             ..update(&Event::Down);
-//             ..update(&Event::Down);
-//             ..update(&Event::Up);
-//         };
-        
-//         assert_eq!(page.list_widget.get_selected_index().value(), 1);
-//     }
-    
-//     #[test]
-//     fn test_update_down() {
-//         let page = cascade! {
-//             ListPage::new(10);
-//             ..update(&Event::Down);
-//         };
-        
-//         assert_eq!(page.list_widget.get_selected_index().value(), 1);
-//     }
-    
-//     #[test]
-//     fn test_update_noop() {
-//         let mut page = ListPage::new(10);
-//         let event = Event::Noop;
-        
-//         let previous_text = page.search_widget.text().to_string();
-//         let previous_index = page.list_widget.get_selected_index().value();
-
-//         page.update(&event);
-        
-//         assert_eq!(page.search_widget.text(), previous_text);
-//         assert_eq!(page.list_widget.get_selected_index().value(), previous_index);
-//     }
-// }
+    #[test]
+    fn test_new() {
+        let list_page = ListPage::new(&[]);
+        assert_eq!(list_page.search_widget.text(), "");
+        assert_eq!(list_page.list_widget.get_selected(), None);
+    }
+}
