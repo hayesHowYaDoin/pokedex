536d310a9dda7e84897398da8f6053494aeaacde
Jordan Hayes <jordan.hayes@plexus.com>
2024-07-31 21:29:37 +0000

    Added render for pokemon stats chart in detail page

---
+++ src/shell/ratatui/components/stat_chart.rs
@@ -0,0 +1,30 @@
+use ratatui::{
+    prelude::{Rect, Style},
+    style::Stylize,
+    terminal::Frame,
+    widgets::{BarChart, Block},
+};
+
+use crate::core::ui::components::PokemonStatChart;
+use super::TuiComponent;
+
+impl TuiComponent for PokemonStatChart {
+    fn render(&self, frame: &mut Frame, layout: &Rect) {
+        let data: Vec<(&str, u64)> = self.get_fields().iter().map(|field| {
+            (field.get_label(), field.get_value() as u64)
+        }).collect();
+        
+        let chart = BarChart::default()
+            .block(Block::bordered().title("Statistics"))
+            .bar_width(8)
+            .bar_gap(2)
+            .group_gap(3)
+            .bar_style(Style::new().blue())
+            .value_style(Style::new().white())
+            .label_style(Style::new().white().italic())
+            .data(&data)
+            .max(self.get_max_value() as u64);
+    
+        frame.render_widget(chart, *layout);
+    }
+}
