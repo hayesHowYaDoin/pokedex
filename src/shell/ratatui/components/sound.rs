a924c8285edd8783a29cb51aabe59822a71e517a
hayesHowYaDoin <jordanhayes98@gmail.com>
2025-01-12 21:28:33 -0700

    Added sound-playing thread

--- src/shell/ratatui/components/sound.rs
+++ src/shell/ratatui/components/sound.rs
@@ -1,5 +1,6 @@
-use std::{thread, time::Duration};
+use std::time::Duration;
 
+use color_eyre::Result;
 use fyrox_resource::io::FsResourceIo;
 use fyrox_sound::buffer::SoundBufferResourceExtension;
 use fyrox_sound::{
@@ -9,10 +10,74 @@ use fyrox_sound::{
     pool::Handle,
     source::{SoundSource, SoundSourceBuilder, Status},
 };
+use tokio::task::JoinHandle;
+use tokio_util::sync::CancellationToken;
 
 use crate::core::ui::components::Sound;
 
-pub fn play_sound(sound: &Sound) {
+#[derive(Debug)]
+pub struct SoundPlayer {
+    sound: Sound,
+    task: JoinHandle<()>,
+    cancellation_token: CancellationToken,
+}
+
+impl SoundPlayer {
+    pub fn new(sound: Sound) -> Result<Self> {
+        let task = tokio::spawn(async {});
+        let cancellation_token = CancellationToken::new();
+
+        Ok(SoundPlayer {
+            sound,
+            task,
+            cancellation_token,
+        })
+    }
+
+    pub fn play(&mut self) {
+        self.cancel();
+        self.cancellation_token = CancellationToken::new();
+        let _cancellation_token = self.cancellation_token.clone();
+
+        let sound = self.sound.clone();
+        self.task = tokio::spawn(async move {
+            loop {
+                tokio::select! {
+                    _ = _cancellation_token.cancelled() => {
+                        break;
+                    }
+                    _ = tokio::time::sleep(Duration::from_millis(500)) => {
+                        play_sound(&sound);
+                        break;
+                    },
+                }
+            }
+        });
+    }
+
+    pub fn stop(&self) -> Result<()> {
+        self.cancel();
+        let mut counter = 0;
+        while !self.task.is_finished() {
+            std::thread::sleep(Duration::from_millis(1));
+            counter += 1;
+            if counter > 50 {
+                self.task.abort();
+            }
+            if counter > 100 {
+                log::error!("Failed to abort SoundPlayer in 100 milliseconds for unknown reason");
+                break;
+            }
+        }
+        Ok(())
+    }
+
+    pub fn cancel(&self) {
+        self.cancellation_token.cancel();
+    }
+}
+
+fn play_sound(sound: &Sound) {
     // Initialize sound engine with default output device.
     let engine = SoundEngine::new().unwrap();
 
@@ -45,7 +110,4 @@ pub fn play_sound(sound: &Sound) {
     // Each sound sound must be added to context, context takes ownership on source
     // and returns pool handle to it by which it can be accessed later on if needed.
     let _source_handle: Handle<SoundSource> = context.state().add_source(source);
-
-    // Wait until sound will play completely.
-    thread::sleep(Duration::from_secs(1));
 }
--- src/shell/ratatui/pages/detail.rs
+++ src/shell/ratatui/pages/detail.rs
@@ -1,6 +1,6 @@
 use std::{
-    sync::LazyLock,
     default::Default,
+    sync::LazyLock,
 };
 
 use color_eyre::eyre::Result;
@@ -17,7 +17,7 @@ use crate::shell::ratatui::components::{
     TuiStatefulComponent,
     image_box::TuiImageBox,
     metadata_box::TuiMetadataBox,
-    sound::play_sound,
+    sound::SoundPlayer,
     stat_chart::TuiPokemonStatChart,
     text_box::TuiTextBox,
     types_box::TuiTypesBox,
@@ -65,9 +65,24 @@ LazyLock::new(|| Layout::default()
         Constraint::Length(30),
     ]));
 
-impl DetailPage {
-    pub fn on_enter(&self) {
-        play_sound(&self.launch_sound);
+#[derive(Debug)]
+pub struct TuiDetailPage {
+    pub page: DetailPage,
+    pub sound_thread: SoundPlayer,
+}
+
+impl TuiDetailPage {
+    pub fn new(page: DetailPage) -> Result<Self> {
+        let sound_thread = SoundPlayer::new(page.get_launch_sound().clone())?;
+        Ok(Self{ page, sound_thread })
+    }
+
+    pub fn set_page(&mut self, page: DetailPage) {
+        self.page = page;
+    }
+
+    pub fn on_enter(&mut self) {
+        self.sound_thread.play();
     }
 
     pub fn render<B: Backend>(&mut self, terminal: &mut Terminal<B>, picker: &mut Picker) -> Result<()> {
@@ -80,43 +95,43 @@ impl DetailPage {
 
             // Title
             TuiTextBox::new(
-                self.get_title_box().to_owned(),
+                self.page.get_title_box().to_owned(),
                 Block::default()
             ).render(frame, &outer_vertical_layout[0]);
 
             // Image
             TuiImageBox::new(
-                self.get_image_box(),
+                self.page.get_image_box(),
                 picker,
             ).render_mut(frame, &inner_first_horizontal_layout[0]);
 
             // Description
             TuiTextBox::new(
-                self.get_description_box().to_owned(),
+                self.page.get_description_box().to_owned(),
                 Block::bordered().title("Description"),
             ).render(frame, &inner_right_vertical_layout[0]);
 
             // Metadata
             TuiMetadataBox::new(
-                self.get_metadata_box().to_owned(),
+                self.page.get_metadata_box().to_owned(),
                 Block::bordered()
             ).render(frame, &inner_right_vertical_layout[1]);
 
             // Stats
             TuiPokemonStatChart::new(
-                self.get_stat_chart().to_owned(),
+                self.page.get_stat_chart().to_owned(),
                 Block::bordered().title("Stats")
             ).render(frame, &inner_second_horizontal_layout[0]);
 
             // Types
             TuiTypesBox::new(
-                self.get_types_box().to_owned(),
+                self.page.get_types_box().to_owned(),
                 Block::bordered().title("Types")
             ).render(frame, &inner_third_horizontal_layout[0]);
 
             // Weaknesses
             TuiTypesBox::new(
-                self.get_weaknesses_box().to_owned(),
+                self.page.get_weaknesses_box().to_owned(),
                 Block::bordered().title("Weaknesses")
             ).render(frame, &inner_third_horizontal_layout[1]);
 
@@ -130,4 +145,3 @@ impl DetailPage {
         Ok(())
     }
 }
-    
