48c400e347e89c9d96091b285dc79f336a5cda65
hayesHowYaDoin <jordanhayes98@gmail.com>
2025-02-05 17:28:13 -0700

    Initial efforts to make nix/debian packages

--- nix/modules/rust.nix
+++ nix/modules/rust.nix
@@ -1,4 +1,4 @@
-{ inputs, ... }:
+{ inputs, pkgs, ... }:
 
 {
   imports = [
@@ -9,19 +9,46 @@
   ];
 
   perSystem = { config, self', pkgs, lib, ... }: {
-    rust-project.crates."rich_pokedex".crane.args = {
-      nativeBuildInputs = with pkgs; [ pkg-config ];
-      # buildInputs = with pkgs; [ alsa-lib.dev udev.dev ];
-      buildInputs = with pkgs; [
-        alsa-lib.dev
-        udev.dev
-        alsa-utils
-      ] ++ lib.optionals pkgs.stdenv.isDarwin (
-        with pkgs.darwin.apple_sdk.frameworks; [
-          IOKit
-        ]
-      );
+    rust-project = {
+      crates."rich_pokedex".crane.args = {
+        nativeBuildInputs = with pkgs; [
+          makeWrapper
+          pkg-config
+        ];
+
+        buildInputs = with pkgs;
+          [ alsa-lib.dev udev.dev alsa-utils ]
+          ++ lib.optionals pkgs.stdenv.isDarwin
+            (with pkgs.darwin.apple_sdk.frameworks; [ IOKit ]);
+      };
+
+      src = lib.cleanSourceWith {
+        src = inputs.self; # The original, unfiltered source
+        filter = path: type:
+          (lib.hasInfix "/data/" path) ||
+          # Default filter from crane (allow .rs files)
+          (config.rust-project.crane-lib.filterCargoSources path type);
+      };
     };
-    packages.default = self'.packages.rich_pokedex;
+
+    packages.default = self'.packages.rich_pokedex.overrideAttrs (oldAttrs: {
+      installPhase =
+        (oldAttrs.installPhase or "") + ''
+          mkdir -p $out/bin/data/assets
+          cp -r ./data/assets/* $out/bin/data/assets/
+          cp -r ./data/pokedex.sqlite $out/bin/data/pokedex.sqlite
+        '';
+
+      postFixup =
+        # HACK: The Linux desktop app is unable to locate the assets
+        # directory, but it does look into the current directory.
+        # So, `cd` to the directory containing assets (which is
+        # `bin/`, per the installPhase above) before launching the
+        # app.
+        (oldAttrs.postFixup or "") + ''
+          wrapProgram $out/bin/${oldAttrs.pname} \
+            --chdir $out/bin
+        '';
+    });
   };
 }
