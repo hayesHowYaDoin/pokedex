81c3b49476e07620ecbc58c4827028da0c244fff
hayesHowYaDoin <jordanhayes98@gmail.com>
2025-08-09 22:07:26 +0000

    Replaced orphan ruled from method for PokemonTableEntry with free function

--- lib/ui-shell/src/state.rs
+++ lib/ui-shell/src/state.rs
@@ -2,18 +2,10 @@ use ui_core::PageState;
 use crate::pages::TuiPage;
 
 pub trait TuiState {
-    fn as_tui_page(&self) -> &dyn TuiPage;
     fn as_tui_page_mut(&mut self) -> &mut dyn TuiPage;
 }
 
 impl TuiState for PageState {
-    fn as_tui_page(&self) -> &dyn TuiPage {
-        match self {
-            PageState::List(page) => page,
-            PageState::Detail(page) => page,
-        }
-    }
-    
     fn as_tui_page_mut(&mut self) -> &mut dyn TuiPage {
         match self {
             PageState::List(page) => page,
