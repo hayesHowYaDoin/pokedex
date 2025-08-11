adefde79ad879391ef55ea06f0d6aa785da7d8b6
hayesHowYaDoin <jordanhayes98@gmail.com>
2025-08-11 01:19:49 +0000

    Moved to global singleton Settings via LazyLock

--- lib/settings/src/lib.rs
+++ lib/settings/src/lib.rs
@@ -1,46 +1,58 @@
 use std::path::{Path, PathBuf};
-use std::sync::LazyLock;
+use std::sync::OnceLock;
 
-static LOG_PATH: LazyLock<PathBuf> =
-    LazyLock::new(|| match std::env::var("POKEDEX_LOG_PATH").ok() {
-        Some(p) => PathBuf::from(p.trim_end_matches('/')),
-        None => PathBuf::from("/usr/share/rich_pokedex/pokedex.log"),
-    });
+static SETTINGS: OnceLock<Settings> = OnceLock::new();
 
-pub static DATABASE_PATH: LazyLock<PathBuf> =
-    LazyLock::new(|| match std::env::var("POKEDEX_DATABASE_PATH").ok() {
-        Some(p) => PathBuf::from(p.trim_end_matches('/')),
-        None => PathBuf::from("/usr/share/rich_pokedex/pokedex.db"),
-    });
-
-pub static ASSETS_PATH: LazyLock<PathBuf> =
-    LazyLock::new(|| match std::env::var("POKEDEX_ASSETS_PATH").ok() {
-        Some(p) => PathBuf::from(p.trim_end_matches('/')),
-        None => PathBuf::from("/usr/share/rich_pokedex/assets"),
-    });
-
+#[derive(Debug)]
 pub struct Settings {
+    log_path: PathBuf,
+    database_path: PathBuf,
+    assets_path: PathBuf,
     is_silent: bool,
 }
 
 impl Settings {
-    pub fn new(is_silent: bool) -> Self {
-        Settings { is_silent }
+    pub fn init(is_silent: bool) {
+        let log_path = match std::env::var("POKEDEX_LOG_PATH").ok() {
+            Some(p) => PathBuf::from(p.trim_end_matches('/')),
+            None => PathBuf::from("/usr/share/rich_pokedex/pokedex.log"),
+        };
+        let database_path = match std::env::var("POKEDEX_DATABASE_PATH").ok() {
+            Some(p) => PathBuf::from(p.trim_end_matches('/')),
+            None => PathBuf::from("/usr/share/rich_pokedex/pokedex.db"),
+        };
+        let assets_path = match std::env::var("POKEDEX_ASSETS_PATH").ok() {
+            Some(p) => PathBuf::from(p.trim_end_matches('/')),
+            None => PathBuf::from("/usr/share/rich_pokedex/assets"),
+        };
+
+        SETTINGS
+            .set(Settings {
+                log_path,
+                database_path,
+                assets_path,
+                is_silent,
+            })
+            .expect("Settings can only be initialized once");
     }
 
-    pub fn get_log_path() -> &'static Path {
-        (*LOG_PATH).as_path()
+    fn get() -> &'static Settings {
+        SETTINGS.get().expect("Settings have not been initialized")
     }
 
-    pub fn get_database_path() -> &'static Path {
-        (*DATABASE_PATH).as_path()
+    pub fn log_path() -> &'static Path {
+        Settings::get().log_path.as_path()
     }
 
-    pub fn get_assets_path() -> &'static Path {
-        (*ASSETS_PATH).as_path()
+    pub fn database_path() -> &'static Path {
+        Settings::get().database_path.as_path()
     }
 
-    pub fn is_silent(&self) -> bool {
-        self.is_silent
+    pub fn assets_path() -> &'static Path {
+        Settings::get().assets_path.as_path()
+    }
+
+    pub fn is_silent() -> bool {
+        Settings::get().is_silent
     }
 }
